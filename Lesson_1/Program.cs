57c672b57a3ac38e97dd9fb75114024b053ca19c
Тарасов Андрей Владимирович <tarasov_a@kolomenskoe.ru>
2023-09-21 13:38:33 +0300

    Добавили команды для написания кода

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Структура проекта
+// Структура и порядок выполнения программы
+
+/* 
+Многострочный коммент-который не видит программа при компиляции
+*/
+{
+Console.WriteLine("Введите три числа, и узнайте, какое из них имеет максимальное значение ");
+
+Console.Write("Введите три числа нажмите ENTER: ");
+int num1, num2, num3 = int.Parse(Console.ReadLine());
+int maxNum = 0;
+if (num1 > maxNum) maxNum = num1;
+if (num2 > maxNum) maxNum = num2;
+if (num3 > maxNum) maxNum = num3;
+Console.Write("Максимальное значение имеет число " + maxNum);
+}
